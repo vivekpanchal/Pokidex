baece5cdafe48d9bc5ec1c677386f6e42f0fc577
vivek panchal <vivekpanchal64@gmail.com>
2025-03-05 23:04:02 +0530

    Update dependencies and add new logo asset
    Signed-off-by: vivekpanchal <vivekpanchal64@gmail.com>

--- app/src/main/java/com/vivek/pokidex/presentation/ui/screen/PokemonListScreen.kt
+++ app/src/main/java/com/vivek/pokidex/presentation/ui/screen/PokemonListScreen.kt
@@ -1,62 +1,60 @@
 package com.vivek.pokidex.presentation.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.vivek.pokidex.R
 import com.vivek.pokidex.presentation.ui.components.PokemonCard
 import com.vivek.pokidex.presentation.viewModel.PokemonViewModel
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokemonListScreen(
     viewModel: PokemonViewModel = hiltViewModel(),
     onCardClick: (String) -> Unit
 ) {
     val pokemonList = viewModel.pokemonList.collectAsLazyPagingItems()
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "PokeDex",
-                        modifier = Modifier.padding(start = 16.dp),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.headlineLarge,
-                        fontWeight = FontWeight.Bold,
-                    )
-                },
+    Surface {
+
+        Column {
+            Spacer(modifier = Modifier.size(20.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.pokemon_logo), // Replace with your icon
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.CenterHorizontally)
             )
-        },
-        content = { paddingValues ->
+
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(paddingValues)
                     .background(MaterialTheme.colorScheme.background)
             ) {
                 // Handling states
@@ -93,7 +91,7 @@ fun PokemonListScreen(
                             items(pokemonList.itemCount) { idx ->
                                 pokemonList[idx]?.let { pokemon ->
                                     PokemonCard(pokemon) {
-                                      //  onCardClick(pokemon.name)
+                                        //  onCardClick(pokemon.name)
                                     }
                                 }
                             }
@@ -130,7 +128,7 @@ fun PokemonListScreen(
                 }
             }
         }
-    )
+    }
 }
 
 
